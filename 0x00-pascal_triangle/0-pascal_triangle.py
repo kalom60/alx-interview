f7f860b392b9f16ad27568094b17f26ebbbffbfd
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-16 15:34:46 +0300

    sticking to old method

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,8 +2,20 @@
 """python module for pascal triangle"""
 
 
+def factorial(n):
+    """Calculate factorial of a number"""
+    if n in [0, 1]:
+        return 1
+    if n == 2:
+        return 2
+    return n * factorial(n - 1)
+
+
 def pascal_triangle(n):
-    """pascal triangle"""
-    value = [[str(11 ** num)] for num in range(n)]
-    val = [num2 for num in value for num2 in num]
-    return val
+    """evaluate and return pascal triangle"""
+    if n <= 0:
+        return []
+    pascal = [[int(factorial(num) / (factorial(num - num1) * factorial(num1)))
+               for num1 in range(num+1)] for num in range(n)]
+
+    return pascal
