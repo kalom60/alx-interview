0cca1cedc8bdd04aa954a5500ea52e08d94cc108
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-29 15:50:22 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,23 +6,15 @@ def minOperations(n):
     """
     return the min operation that takes to write h n times
     """
-    cp = 0
-    h = [x for x in range(1, n) if n % x == 0]
-    hcls = [max(h)]
-    if hcls[0] == 1:
-        return 0
-
-    while True:
-        if hcls[-1] != 1:
-            hcl = max(x for x in range(1, hcls[-1]) if hcls[-1] % x == 0)
-            hcls.append(hcl)
-        else:
-            break
-
-    for x in range(1, len(h) + 1):
-        if x in hcls:
+    cp = last = 0
+    h = 'H'
+    while len(h) < n:
+        if n % len(h) == 0:
+            last = len(h)
+            h = h * 2
             cp += 2
         else:
+            h += last * 'H'
             cp += 1
 
     return cp
