4f4b8581e3a6bd1d0b7b22850264e7eed0757f39
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-07 23:40:46 +0300

    use simple validator

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,70 +1,44 @@
 #!/usr/bin/python3
-"""a script that reads stdin line by line and computes metrics"""
+"""script that reads stdin line by line and computes metrics"""
+
 import sys
-from datetime import datetime
-
-if __name__ == '__main__':
-    status_code = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
-
-checker = {
-    'method': '"GET /projects/260 HTTP/1.1"',
-}
-
-file_size = 0
-on_ten = 0
 
 
-def check_ip(ip):
-    """validate ip address"""
-    ip = ip.split('.')
-    if len(ip) == 4:
-        for addy in ip:
-            if addy.isdigit() and \
-                    (int(addy) > 0 and int(addy) < 255):
-                return True
-            return False
-    return False
+if __name__ == "__main__":
+    status_code = {"200": 0,
+                   "301": 0,
+                   "400": 0,
+                   "401": 0,
+                   "403": 0,
+                   "404": 0,
+                   "405": 0,
+                   "500": 0
+                   }
+    on_ten = 0
+    file_size = 0
 
-
-def display_metrics():
-    """display the metrics"""
-    print(f'File size: {file_size}')
-    for k, v in status_code.items():
-        if v:
-            print(f'{k}: {v}')
-
-
-try:
-    for lines in sys.stdin:
-        line = lines.split(' ')
-        ip = line[0]
-        dates = line[2][1:] + line[3][:-1]
-        dates = datetime.strptime(dates, "%Y-%m-%d%H:%M:%S.%f")
-        minus = line[1]
-        method = line[4] + ' ' + line[5] + ' ' + line[6]
-        code = line[7]
-        size = line[-1].split('\\')[0]
-        if minus == '-' and code in status_code and \
-                method == checker['method'] and check_ip(ip) \
-                    and isinstance(dates, datetime):
-            if on_ten == 10:
-                display_metrics()
-                on_ten = 0
+    def parse_line(line):
+        """ parse a line"""
+        parsed_line = line.split(' ')
+        code = parsed_line[-2]
+        if code in status_code:
             status_code[code] += 1
-            file_size += int(size)
+        return int(parsed_line[-1])
+
+    def print_stats():
+        """display the stat"""
+        print(f'File size: {file_size}')
+        for k, v in status_code.items():
+            if v:
+                print(f'{k}: {v}')
+
+    try:
+        for line in sys.stdin:
+            file_size += parse_line(line)
             on_ten += 1
-        else:
-            print(ip, minus, dates, method, code, size)
-except KeyboardInterrupt:
-    display_metrics()
-else:
-    display_metrics()
+            if on_ten == 10:
+                print_stats()
+                on_ten = 0
+    except KeyboardInterrupt:
+        print_stats()
+        sys.exit(1)
