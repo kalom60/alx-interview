ec3601fe7b2bc7a28a3131879be090bfdac0b4b6
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-07 23:45:11 +0300

    use raise

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+"""
+Task - Script that reads stdin line by line and computes metrics
+"""
 
 import sys
 
@@ -12,33 +14,35 @@ if __name__ == "__main__":
                    "403": 0,
                    "404": 0,
                    "405": 0,
-                   "500": 0
-                   }
-    on_ten = 0
+                   "500": 0}
+    on_ten = 1
     file_size = 0
 
     def parse_line(line):
-        """ parse a line"""
-        parsed_line = line.split(' ')
-        code = parsed_line[-2]
-        if code in status_code:
-            status_code[code] += 1
-        return int(parsed_line[-1])
+        """parse a line"""
+        try:
+            parsed_line = line.split()
+            code = parsed_line[-2]
+            if code in status_code.keys():
+                status_code[code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
 
     def print_stats():
         """display the stat"""
-        print(f'File size: {file_size}')
-        for k, v in status_code.items():
-            if v:
-                print(f'{k}: {v}')
+        print("File size: {}".format(file_size))
+        for key in status_code:
+            if status_code[key]:
+                print("{}: {}".format(key, status_code[key]))
 
     try:
         for line in sys.stdin:
             file_size += parse_line(line)
-            on_ten += 1
-            if on_ten == 10:
+            if on_ten % 10 == 0:
                 print_stats()
-                on_ten = 0
+            on_ten += 1
     except KeyboardInterrupt:
         print_stats()
-        sys.exit(1)
+        raise
+    print_stats()
