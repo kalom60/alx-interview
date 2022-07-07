0b0102b3943e544911c4c496b27d298da66dd837
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-07 23:18:07 +0300

    add __name__

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """a script that reads stdin line by line and computes metrics"""
+import sys
+from datetime import datetime
 
-
-status_code = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0,
-}
+if __name__ == '__main__':
+    status_code = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
 
 checker = {
     'method': '"GET /projects/260 HTTP/1.1"',
@@ -27,7 +29,7 @@ def check_ip(ip):
     if len(ip) == 4:
         for addy in ip:
             if addy.isdigit() and \
-                    (int(addy) < 0 and int(addy) > 255):
+                    (int(addy) > 0 and int(addy) < 255):
                 return True
             return False
     return False
@@ -42,7 +44,7 @@ def display_metrics():
 
 
 try:
-    for lines in input():
+    for lines in sys.stdin:
         line = lines.split(' ')
         ip = line[0]
         dates = line[2][1:] + line[3][:-1]
@@ -60,6 +62,8 @@ try:
             status_code[code] += 1
             file_size += int(size)
             on_ten += 1
+        else:
+            print(ip, minus, dates, method, code, size)
 except KeyboardInterrupt:
     display_metrics()
 else:
