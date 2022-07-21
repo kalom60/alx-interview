9bd7ffaf99d2bb7138845c1052bd2a2571d19563
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-07-21 19:29:22 +0300

    nqueens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""nquees task"""
+import sys
+
+
+def nqueens(n):
+    """find the nqueens for the size of n"""
+    if n < 4:
+        print('N must be at least 4')
+        return
+    column = []
+    diagX = []
+    diagY = []
+
+    result = []
+
+    def backtrack(row):
+        """find nqueens using backtracking"""
+        if row == n:
+            print(result)
+            return
+
+        for col in range(n):
+            if col in column or (row + col) in diagX or (row - col) in diagY:
+                continue
+            column.append(col)
+            diagX.append(row + col)
+            diagY.append(row - col)
+            result.append([row, col])
+
+            backtrack(row + 1)
+
+            column.remove(col)
+            diagX.remove(row + col)
+            diagY.remove(row - col)
+            result.remove([row, col])
+    backtrack(0)
+
+
+size = sys.argv[1]
+try:
+    nqueens(int(size))
+except ValueError:
+    print('N must be a number')
