1eee26349dff5340186376fe6a24f207ba0608db
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-11 22:03:34 +0300

    using counter

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,13 +5,14 @@
 def makeChange(coins, total):
     """Return: fewest number of coins needed to meet total"""
     if total > 0:
-        amount = [total + 1 for _ in range(total + 1)]
-        amount[0] = 0
-        for a in range(1, total + 1):
-            for c in coins:
-                if a - c >= 0:
-                    amount[a] = min(amount[a], 1 + amount[a - c])
-        if amount[total] != total + 1:
-            return amount[total]
-        return -1
+        count = 0
+        coins = sorted(coins)
+        coins.reverse()
+        for coin in coins:
+            while(total >= coin):
+                count += 1
+                total -= coin
+        if total != 0:
+            return -1
+        return count
     return 0
