fdb1a91506f56fb233fb0396862ce095265360df
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-18 22:13:07 +0300

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Island Perimeter script"""
+
+
+def island_perimeter(grid):
+    """return perimeter of island"""
+    visited = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if ((j - 1) >= 0) or ((j + 1) <= len(grid[i])):
+                    try:
+                        if grid[i][j - 1] == 0:
+                            visited += 1
+                        if grid[i][j + 1] == 0:
+                            visited += 1
+                    except IndexError:
+                        visited += 1
+                if ((i - 1) >= 0) or ((i + 1) <= len(grid)):
+                    try:
+                        if grid[i - 1][j] == 0:
+                            visited += 1
+                        if grid[i + 1][j] == 0:
+                            visited += 1
+                    except IndexError:
+                        visited += 1
+    return visited
