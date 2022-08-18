87b464726fcfee0a9e8baf59447874b605a1259d
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-18 22:18:33 +0300

    docstring

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""Island Perimeter module"""
 
 
 def island_perimeter(grid):
-    """"""
+    """return perimeter of island"""
     visited = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
